9c9b68e7737249c4f246bce47ad2459e01ca367c
Milanozaki <milano23zak@gmail.com>
2024-10-02 16:22:17 +0700

    logoutkasir

--- app/(authenticated)/kasirapp/layout.tsx
+++ app/(authenticated)/kasirapp/layout.tsx
@@ -6,13 +6,13 @@ import { HistoryOutlined } from "@ant-design/icons";
 import { IoFastFoodOutline } from "react-icons/io5";
 import { Avatar, Dropdown, Button, Divider } from "antd";
 import Image from "next/image";
-import { useRouter,usePathname } from "next/navigation";
+import { usePathname,useRouter} from "next/navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
   const [selectedMenu, setSelectedMenu] = useState<string>("");
   const pathname = usePathname();
-  const router = useRouter(); // Inisialisasi router
+  const router = useRouter();
   const authenticatedMenu = [
     { name: "Menu", path: "/kasirapp/menu", icon: <IoFastFoodOutline size={20} /> },
     { name: "Riwayat", path: "/kasirapp/riwayat", icon: <HistoryOutlined size={20} /> },
@@ -35,8 +35,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         danger
         className="w-full -mt-4"
         onClick={() => {
+          console.log("Logout button clicked");
           localStorage.removeItem('userEmail');
-          router.push("/login_kasir");
+          router.push('/login_kasir');
         }}
       >
         Keluar
