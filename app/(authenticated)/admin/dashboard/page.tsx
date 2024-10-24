4b10dfe266fa3b9c8aa211580319e3c758638b40
Parsyaaa14 <muhammadparsya1@gmail.com>
2024-10-24 11:49:54 +0700

    mengefetch monthly sales by id_toko

--- app/(authenticated)/admin/dashboard/page.tsx
+++ app/(authenticated)/admin/dashboard/page.tsx
@@ -37,88 +37,34 @@ interface Produk {
   stok: number;
 }
 
+const Period = {
+  TODAY: "today",
+  YESTERDAY: "yesterday",
+  THIS_WEEK: "this_week",
+  LAST_WEEK: "last_week",
+  THIS_MONTH: "this_month",
+  LAST_MONTH: "last_month",
+  LAST_30_DAYS: "last_30_days",
+  THIS_QUARTER: "this_quarter",
+  LAST_QUARTER: "last_quarter",
+  THIS_YEAR: "this_year",
+  LAST_YEAR: "last_year",
+};
+
 // DashboardPage Component
 const DashboardPage = () => {
   const [produk, setProduk] = useState<Produk[]>([]);
-  const [jumlahTransaksi, setJumlahTransaksi] = useState<number>(0);
-  const [totalOmset, setTotalOmset] = useState<number>(0);
+  const [jumlahTransaksi, setJumlahTransaksi] = useState(0); // State untuk menyimpan jumlah transaksi
+  const [error, setError] = useState<string | null>(null); // Ubah tipe state menjadi string atau null
+  const [totalHarga, setTotalHarga] = useState(0);
+  const [period, setPeriod] = useState(Period.THIS_MONTH); // Default periode
   const [selectedYear, setSelectedYear] = useState<number>(
     new Date().getFullYear()
   );
+  const [monthlySales, setMonthlySales] = useState<MonthlySales[]>([]);
   const [produkCount, setProdukCount] = useState(0);
   const idToko = localStorage.getItem("id_toko"); // Ambil id_toko dari localStorage
-  const [loading, setLoading] = useState<boolean>(true);
-  const [chartData, setChartData] = useState<any>({
-    labels: [],
-    datasets: [
-      {
-        label: "Penjualan",
-        data: [],
-        backgroundColor: "rgba(51, 47, 208, 1)",
-        borderColor: "rgba(51, 47, 208, 1)",
-        borderWidth: 1,
-      },
-    ],
-  }); // Inisialisasi chartData dengan struktur yang benar
   const router = useRouter(); // hook untuk navigasi
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-
-        // const stokResponse = await fetch(
-        //   "http://localhost:3222/produk/filter-stok"
-        // );
-
-        const transaksiResponse = await fetch(
-          "http://localhost:3222/transaksi/count"
-        );
-        const transaksiData = await transaksiResponse.json();
-        setJumlahTransaksi(transaksiData.jumlahTransaksi);
-
-        const omsetResponse = await fetch(
-          "http://localhost:3222/transaksi/total-harga"
-        );
-        const omsetData = await omsetResponse.json();
-        setTotalOmset(omsetData);
-
-        // Fetch data penjualan bulanan
-        const monthlySalesResponse = await fetch(
-          "http://localhost:3222/transaksi/monthly-sales"
-        );
-        const monthlySalesData: MonthlySales[] =
-          await monthlySalesResponse.json();
-
-        // Format data untuk chart
-        const labels = monthlySalesData.map((item) => {
-          const date = new Date(item.month);
-          return date.toLocaleString("default", { month: "long" }); // Mengambil nama bulan
-        });
-        const salesData = monthlySalesData.map((item) => item.total); // Ambil total penjualan
-
-        // Set data chart
-        setChartData({
-          labels,
-          datasets: [
-            {
-              label: "Penjualan",
-              data: salesData,
-              backgroundColor: "rgba(51, 47, 208, 1)",
-              borderColor: "rgba(51, 47, 208, 1)",
-              borderWidth: 1,
-            },
-          ],
-        });
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [router]);
 
   const fetchProdukCount = async () => {
     try {
@@ -147,29 +93,76 @@ const DashboardPage = () => {
       });
   }, [idToko]);
 
-  // Fungsi untuk mengambil nilai dari cookie
-  // const getCookie = (name: string) => {
-  //   const value = `; ${document.cookie}`;
-  //   const parts = value.split(`; ${name}=`);
-  //   if (parts.length === 2) return parts.pop()?.split(";").shift();
-  //   return null;
-  // };
+  useEffect(() => {
+    // Ambil id_toko dari localStorage
+    const id_toko = localStorage.getItem("id_toko");
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+    if (!id_toko) {
+      console.error("id_toko not found in localStorage");
+      setError("id_toko not found");
+      return;
+    }
 
-  const chartOptions = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top" as const,
+    // Fetch jumlah transaksi dari API menggunakan Axios
+    axios
+      .get(`http://localhost:3222/transaksi/count?id_toko=${id_toko}`)
+      .then((response) => {
+        setJumlahTransaksi(response.data.jumlahTransaksi); // Simpan data jumlah transaksi ke state
+      })
+      .catch((error) => {
+        console.error("Error fetching jumlah transaksi:", error);
+        setError("Error fetching data"); // Set error jika ada masalah
+      });
+  }, []); // useEffect dijalankan sekali setelah komponen dipasang
+
+  useEffect(() => {
+    if (idToko) {
+      axios
+        .get(
+          `http://localhost:3222/transaksi/total-harga?id_toko=${idToko}&period=${period}`
+        )
+        .then((response) => {
+          setTotalHarga(response.data); // Simpan totalHarga yang diterima
+        })
+        .catch((error) => {
+          console.error("Error fetching total harga:", error);
+        });
+    }
+  }, [idToko, period]); // Tambahkan period ke dalam dependency array
+
+  useEffect(() => {
+    const fetchMonthlySales = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3222/transaksi/monthly-sales?id_toko=${idToko}`
+        );
+        setMonthlySales(response.data); // Simpan data penjualan bulanan yang diterima
+      } catch (error) {
+        console.error("Error fetching monthly sales:", error);
+      }
+    };
+
+    if (idToko) {
+      fetchMonthlySales(); // Panggil fungsi fetch jika id_toko tersedia
+    } else {
+      console.error("id_toko is not found in localStorage.");
+    }
+  }, [idToko]); // Dependency array untuk re-fetch saat idToko berubah
+
+  const data = {
+    labels: monthlySales.map((sale) => {
+      const date = new Date(`${sale.month}-01`); // Mengonversi 'YYYY-MM' menjadi Date object
+      return new Intl.DateTimeFormat("id-ID", { month: "long" }).format(date); // Menghasilkan nama bulan dalam bahasa Indonesia
+    }),
+    datasets: [
+      {
+        label: "Total Penjualan",
+        data: monthlySales.map((sale) => sale.total),
+        backgroundColor: "rgba(51, 47, 208, 1)",
+        borderColor: "rgba(51, 47, 208, 1)",
+        borderWidth: 1,
       },
-      title: {
-        display: true,
-        text: "Statistik Penjualan Bulanan",
-      },
-    },
+    ],
   };
 
   return (
@@ -244,26 +237,59 @@ const DashboardPage = () => {
       {/* Baris kedua */}
       <div className="grid grid-cols-[3fr,1fr] gap-5 mr-5 ml-5 mt-10">
         {/* Card Statistik Penjualan Bulanan */}
-        <div className="w-full p-6 shadow-xl hover:shadow-2xl transition-shadow duration-300 rounded-xl relative flex flex-col h-full">
-          <h2 className="text-xl font-semibold text-center bg-[#294978] absolute top-0 left-0 right-0 text-white p-3 rounded-t-xl">
-            Statistik Penjualan Bulanan
+        <div className="container mx-auto mt-10">
+          <h2 className="text-2xl font-bold text-center mb-4">
+            Penjualan Bulanan
           </h2>
-          <div className="flex justify-center items-center h-full">
-            {chartData.labels.length > 0 ? ( // Pastikan ada data sebelum render
-              <Bar data={chartData} options={chartOptions} height={130} />
-            ) : (
-              <div>Loading chart data...</div>
-            )}
-          </div>
+          {monthlySales.length > 0 ? (
+            <Bar
+              data={data}
+              options={{
+                responsive: true,
+                plugins: {
+                  legend: {
+                    position: "top",
+                  },
+                  title: {
+                    display: true,
+                    text: "Grafik Penjualan Bulanan",
+                  },
+                },
+              }}
+            />
+          ) : (
+            <div className="text-center">Loading data...</div>
+          )}
         </div>
 
         <div className="w-full h-full bg-gradient-to-r from-[#fc7a4a] to-[#ff6a34] p-6 shadow-xl hover:shadow-2xl transition-shadow duration-300 rounded-xl relative flex flex-col animate-pulseScale animate-glow">
-          <h2 className="text-xl font-semibold text-center text-white ">
-            Pendapatan Anda
-          </h2>
+          <div className="flex flex-col items-center">
+            <h2 className="text-xl font-semibold text-center text-white mb-4">
+              Pendapatan Anda
+            </h2>
+            <select
+              id="period"
+              value={period}
+              onChange={(e) => setPeriod(e.target.value)} // Mengubah periode saat dropdown berubah
+              className="block w-8/12 p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500 focus:border-blue-500"
+            >
+              <option value={Period.TODAY}>Hari Ini</option>
+              <option value={Period.YESTERDAY}>Kemarin</option>
+              <option value={Period.THIS_WEEK}>Minggu Ini</option>
+              <option value={Period.LAST_WEEK}>Minggu Lalu</option>
+              <option value={Period.THIS_MONTH}>Bulan Ini</option>
+              <option value={Period.LAST_MONTH}>Bulan Lalu</option>
+              <option value={Period.LAST_30_DAYS}>30 Hari Terakhir</option>
+              <option value={Period.THIS_QUARTER}>Kuartal Ini</option>
+              <option value={Period.LAST_QUARTER}>Kuartal Lalu</option>
+              <option value={Period.THIS_YEAR}>Tahun Ini</option>
+              <option value={Period.LAST_YEAR}>Tahun Lalu</option>
+            </select>
+          </div>
+
           <div className="flex flex-col justify-center items-center h-full">
             <p className="text-3xl font-bold text-white">
-              Rp {totalOmset.toLocaleString()}
+              Rp {totalHarga.toLocaleString()}
             </p>
             <p className="text-sm text-white">Total Pendapatan Bulan Ini</p>
           </div>
